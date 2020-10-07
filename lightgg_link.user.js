50744490fa99e63daca532c1c593bfa4dbf342aa
Toby Vincent <tobyv13@gmail.com>
2020-10-07 15:32:14 -0500

    fixed image url

--- lightgg_link.user.js
+++ lightgg_link.user.js
@@ -15,7 +15,7 @@ window.onload = (event) => {
     url = window.location.href.replace(/^https:\/\/(?:[^\.]*\.)?destinytracker\.com\/destiny-2\/(db\/items\/\d+)\?perks=(?:\d+,?)+$/, "https://www.light.gg/$1")
 
     var logo = document.createElement("img");
-    logo.setAttribute("src", "https://www.light.gg/Content/Images/new-icon.png");
+    logo.setAttribute("src", "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8a37d87d-a6e5-4ad9-9c67-02a8842ca36b/dbpr9n8-773872ad-74cc-4124-8458-dbce41771acd.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvOGEzN2Q4N2QtYTZlNS00YWQ5LTljNjctMDJhODg0MmNhMzZiXC9kYnByOW44LTc3Mzg3MmFkLTc0Y2MtNDEyNC04NDU4LWRiY2U0MTc3MWFjZC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ._R8hTZ0Uzfh5Pl_BClZ7WgmI7TJugP5-s9r3YOBXo8U");
     logo.setAttribute("title", "View on Light.gg");
     logo.setAttribute("alt", "Light.gg Logo");
     logo.style.width = '2.5%'

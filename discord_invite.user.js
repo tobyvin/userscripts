199a0e54cfceb33f6a4be009fcbb2369121ee124
Toby Vincent <tobyv13@gmail.com>
2021-03-31 18:38:31 -0500

    fixed updateURL

--- discord_invite.user.js
+++ discord_invite.user.js
@@ -4,7 +4,7 @@
 // @description Changes Continue to Discord link to Close Window link on discord invites
 // @author      tobyvin
 // @version     1.0.0
-// @updateURL   https://github.com/tobyvin/userscripts/raw/master/continue_to_discord.js
+// @updateURL   https://github.com/tobyvin/userscripts/raw/master/discord_invite.user.js
 // @match       https://discord.com/invite/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @grant       none
